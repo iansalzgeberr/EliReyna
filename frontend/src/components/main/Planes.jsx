78893d9ce7348ca5065ae874f1abda35244e86ee
iansalzgeberr <94173@sistemas.frc.utn.edu.ar>
2024-07-24 10:57:16 -0300

    agregue los planes que mando la eli

--- frontend/src/components/main/Planes.jsx
+++ frontend/src/components/main/Planes.jsx
@@ -9,39 +9,60 @@ const Planes = () => {
 
   const plans = [
     {
-      title: 'Aumento de masa muscular',
-      price: '15.000',
+      title: 'Aumento masa muscular',
+      price: '20.000',
       description: 'Superá tus límites',
-      content: 'Nuestro plan para aumentar masa muscular está diseñado para ayudarte a ganar músculo de manera efectiva y segura. Este programa combina entrenamiento de resistencia progresivo con una nutrición adecuada para maximizar tus resultados.',
+      content: 'Nuestro plan de aumento de masa muscular te ayudará a ganar fuerza y tamaño muscular con rutinas de entrenamiento intensas y una nutrición adecuada.',
       imageSrc: '../../../images/razones1.jpg' // Reemplaza con la ruta correcta a tu imagen
     },
     {
       title: 'Descenso de peso',
       price: '20.000',
       description: 'Superá tus límites',
-      content: 'Contenido del Plan de Descenso de peso...',
+      content: 'Este plan está diseñado para ayudarte a perder peso de manera saludable mediante una combinación de ejercicios cardiovasculares y una dieta balanceada.',
       imageSrc: '../../../images/razones2.jpeg'
     },
     {
-      title: 'Personalizado',
-      price: '25.000',
+      title: 'Tonificación',
+      price: '20.000',
       description: 'Superá tus límites',
-      content: 'Contenido del Plan Personalizado...',
+      content: 'Nuestro plan de tonificación se centra en definir y fortalecer tus músculos, dándote una apariencia más esculpida y tonificada.',
       imageSrc: '/path/to/image3.jpg'
     },
     {
-      title: 'Tonificación',
-      price: '25.000',
+      title: 'Entrenamiento en casa',
+      price: '20.000',
       description: 'Superá tus límites',
-      content: 'Contenido del Plan de Tonificación...',
+      content: 'Este plan te permite entrenar desde la comodidad de tu hogar con rutinas efectivas que no requieren equipo especializado.',
       imageSrc: '/path/to/image4.jpg'
     },
     {
-      title: 'Entrenamiento en casa',
-      price: '10.000',
+      title: 'Adulto mayores',
+      price: '20.000',
       description: 'Superá tus límites',
-      content: 'Contenido del Plan de Entrenamiento en casa...',
+      content: 'Nuestro plan para adultos mayores está diseñado para mejorar la movilidad, la fuerza y la salud en general, adaptado a las necesidades específicas de esta etapa de la vida.',
       imageSrc: '/path/to/image5.jpg'
+    },
+    {
+      title: 'Plan adaptado a deportistas',
+      price: '20.000',
+      description: 'Superá tus límites',
+      content: 'Este plan está diseñado para deportistas que buscan mejorar su rendimiento en su disciplina específica, con entrenamientos y nutrición adaptados a sus necesidades.',
+      imageSrc: '/path/to/image6.jpg'
+    },
+    {
+      title: 'R.P.G (reeducación postural global) + aumento de masa muscular',
+      price: '20.000',
+      description: 'Superá tus límites',
+      content: 'Combina la reeducación postural global con un plan de aumento de masa muscular para corregir tu postura mientras ganas fuerza y tamaño muscular.',
+      imageSrc: '/path/to/image7.jpg'
+    },
+    {
+      title: 'R.P.G (reeducación postural global) + tonificación',
+      price: '20.000',
+      description: 'Superá tus límites',
+      content: 'Este plan combina la reeducación postural global con ejercicios de tonificación para mejorar tu postura y definir tu musculatura.',
+      imageSrc: '/path/to/image8.jpg'
     },
   ];
 
