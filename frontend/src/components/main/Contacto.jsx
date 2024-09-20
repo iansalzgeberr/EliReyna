db04e2b24f7ca4274d37456f8cb73f365d6f47d2
iansalzgeberr <94173@sistemas.frc.utn.edu.ar>
2024-09-20 14:04:30 -0300

    lo de la nutri solo falta la foto

--- frontend/src/components/main/Contacto.jsx
+++ frontend/src/components/main/Contacto.jsx
@@ -1,17 +1,14 @@
 // src/components/RedesSociales.js
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGoogle, faFacebook, faYoutube, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import './Estilos.css';
 
 const Contacto = () => {
   return (
     <div className="redes-sociales-container">
-      <a href="https://google.com" className="icono" target="_blank" rel="noopener noreferrer">
+      <a href="https://wa.me/+5493517569078" className="icono" target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon icon={faWhatsapp} size="2x" />
-      </a>
-      <a href="https://facebook.com" className="icono" target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faFacebook} size="2x" />
       </a>
       <a href="https://www.instagram.com/elireyna_/" className="icono" target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon icon={faInstagram} size="2x" />
