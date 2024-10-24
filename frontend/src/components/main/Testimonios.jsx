d769b5e86341ebce877c35e675938cf852243be4
iansalzgeberr <94173@sistemas.frc.utn.edu.ar>
2024-10-24 12:31:24 -0300

    solo cambie el nick de ig

--- frontend/src/components/main/Testimonios.jsx
+++ frontend/src/components/main/Testimonios.jsx
@@ -5,7 +5,7 @@ const Testimonios = () => {
   const testimoniosData = [
     { usuario: '@fquintana19', texto: '“Cuando llegue a Legion Fitness, me senti muy bien, fui motivada desde el dia uno. Eli es una gran profesional, muy dedicada a su trabajo y sobre todo siempre con muy buena onda, la verdad estoy agradecida ya que gracias a ella aprendi a entrenar y vi muy buenos resultados”' },
     { usuario: '@tini_veyrand', texto: '“Eli representa un reto, es la persona que te motiva y te guía a que puedas seguir mejorando y evolucionado en el entrenamiento para cumplir tus objetivos! Excelente persona y profesional, además de amiga.”' },
-    { usuario: '@ignacio.dellgado', texto: '“Eli es una excelente entrenadora, me enseñó a corregir técnica para lograr mejores resultados además de ser una ayuda en cuestiones diariarias que hacen al logro de los objetivos es una persona con paciencia y dedicación  por su trabajo.”' },
+    { usuario: '@delfinajalil', texto: '“Eli es una excelente entrenadora, me enseñó a corregir técnica para lograr mejores resultados además de ser una ayuda en cuestiones diariarias que hacen al logro de los objetivos es una persona con paciencia y dedicación  por su trabajo.”' },
     { usuario: '@yaninaa1259', texto: '“Una entrenadora excelente!!! A tu lado logré llevar mi físico y cabeza a una mejor versión... aprendí mucho y sobre todo admiro profundamente tu trabajo”' }
   ];
 
